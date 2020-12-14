1e66c47499e24f40914c0de3e70a90a889a68e46
Burza <burza@iMac-Alina-Pszczokowska.local>
2020-12-14 15:20:43 +0100

    first commit

---
+++ Program.cs
@@ -0,0 +1,112 @@
+﻿using System;
+
+namespace Snake
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            GameBoard gameBoard = new GameBoard();
+            Snake snake = new Snake();
+            
+            char direction = 'R';
+            bool gameOn = true;
+            double gameTickTime = 500;
+
+            byte snakeLength = 3;
+            bool speedAdded = false;
+
+            DateTime prevDate = DateTime.Now;
+
+            gameBoard.drawGameBoard();
+
+            while (gameOn)
+            {
+                Console.CursorVisible = false;
+
+                Console.ForegroundColor = ConsoleColor.White;
+                snake.draw();
+
+                if ((DateTime.Now - prevDate).TotalMilliseconds > gameTickTime)
+                {
+                    snakeLength = snake.move(direction);
+                    prevDate = DateTime.Now;
+                    snake.checkCollisions();
+                    
+
+                }
+
+                // speed handling each 5 bodysegments + 5% speed up to 500%
+                if ((snakeLength % 5) == 0 && gameTickTime > 100 && !speedAdded)
+                {
+                    gameTickTime -= 25;
+                    speedAdded = true;
+                }
+                if(snakeLength%5 != 0)
+                {
+                    speedAdded = false;
+                }
+
+
+                if (Console.KeyAvailable)
+                {
+                    ConsoleKeyInfo input = Console.ReadKey();
+                    Console.WriteLine(input.KeyChar);
+
+
+                    switch (input.Key)
+                    {
+                        case ConsoleKey.Escape:
+                            gameOn = false;
+                            break;
+
+
+                        case ConsoleKey.UpArrow:
+                            if (direction != 'D' && direction != 'U')
+                            {
+                                direction = 'U';
+
+                            }
+                            break;
+                        case ConsoleKey.DownArrow:
+                            if (direction != 'U' && direction != 'D')
+                            {
+                                direction = 'D';
+
+
+                            }
+                            break;
+                        case ConsoleKey.RightArrow:
+                            if (direction != 'L' && direction != 'R')
+                            {
+                                direction = 'R';
+
+
+                            }
+                            break;
+                        case ConsoleKey.LeftArrow:
+                            if (direction != 'R' && direction != 'L')
+                            {
+                                direction = 'L';
+
+                            }
+                            break;
+
+                    }
+
+
+
+                }
+
+
+            }
+
+
+            }
+
+
+        }
+    }
+
+
---
+++ Snake.cs
@@ -0,0 +1,172 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Snake
+{
+    public class Snake
+    {
+        Random random = new Random();
+
+        public List<byte> bodyX = new List<byte>();
+        public List<byte> bodyY = new List<byte>();
+
+        public byte headX;
+        public byte headY;
+
+        public byte fruitX;
+        public byte fruitY;
+
+        public byte gameBoardWidth = 50;
+        public byte gameBoardHeight = 50;
+
+        public byte snakeLength;
+        public GameBoard gameboard;
+
+        public char prevDirection = ' ';
+
+
+        public Snake()
+
+        {
+            headX = 20;
+            headY = 20;
+            snakeLength = 3;
+            bodyX.Add(20);
+            bodyY.Add(20);
+            drawFruit();
+            gameboard = new GameBoard();
+
+        }
+
+
+        public void draw()
+        {
+
+
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.SetCursorPosition(headX, headY);
+            Console.Write("*");
+
+
+            int index = bodyX.Count - 1 - snakeLength;
+
+            if (index >= 0 && index <= bodyX.Count)
+            {
+                Console.SetCursorPosition(bodyX[index], bodyY[index]);
+                Console.Write(" ");
+            }
+        }
+
+        public void drawFruit()
+        {
+
+
+            fruitX = (byte) (random.Next(gameBoardWidth) + 1);
+            fruitY = (byte) (random.Next(gameBoardHeight) + 1);
+
+            Console.SetCursorPosition(fruitX, fruitY);
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            Console.Write("@");
+        }
+
+        
+
+        public void checkCollisions()
+        {
+            //boundaries
+            if((headX == 50 || headY == 50) || (headX == 0 || headY == 0))
+            {
+                gameOver();
+            }
+            //body
+            int index = bodyX.Count - 1 - snakeLength;
+            if (index >= 0 && index <= bodyX.Count)
+            {
+                for (int i = 0; i < snakeLength; i++)
+                {
+                    if (headX == bodyX[index + i] && headY == bodyY[index + i])
+                    {
+                        gameOver();
+                    }
+                }
+            }
+            //fruit
+
+            if(headX == fruitX && headY == fruitY)
+            {
+                snakeLength += 1;
+                drawFruit();
+            }
+
+
+        }
+
+        public void gameOver()
+        {
+            headX = 20;
+            headY = 20;
+            snakeLength = 3;
+            bodyX = new List<byte>();
+            bodyY = new List<byte>();
+            bodyX.Add(20);
+            bodyY.Add(20);
+            prevDirection = ' ';
+            Console.Clear();
+            gameboard.drawGameBoard();
+            drawFruit();
+        }
+
+
+        public byte move(char direction)
+        {
+            
+            if (direction == 'U' && prevDirection == 'D')
+            {
+                direction = prevDirection;
+            }
+            else if (direction == 'D' && prevDirection == 'U')
+            {
+                direction = prevDirection;
+            }
+            else if (direction == 'L' && prevDirection == 'R')
+            {
+                direction = prevDirection;
+            }
+            else if (direction == 'R' && prevDirection == 'L')
+            {
+                direction = prevDirection;
+            }
+
+
+            switch (direction)
+            {
+                case 'U':
+                    headY -= 1;
+                    prevDirection = direction;
+                    break;
+                case 'D':
+                    headY += 1;
+                    prevDirection = direction;
+                    break;
+                case 'L':
+                    headX -= 1;
+                    prevDirection = direction;
+                    break;
+                case 'R':
+                    headX += 1;
+                    prevDirection = direction;
+                    break;
+            }
+
+
+            
+
+            bodyX.Add(headX);
+            bodyY.Add(headY);
+            return snakeLength;
+        }
+
+
+    }
+}
