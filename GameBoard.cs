a0e289724789d691a462811ea4c93776d02ba2ff
Burza <burza@iMac-Alina-Pszczokowska.local>
2020-12-14 15:23:13 +0100

    commit

---
+++ GameBoard.cs
@@ -0,0 +1,50 @@
+﻿using System;
+namespace Snake
+{
+    public class GameBoard
+    {
+
+        public byte gameBoardWidth;
+        public byte gameBoardHeight;
+
+        public GameBoard()
+        {
+            gameBoardWidth = 50;
+            gameBoardHeight = 50;
+
+        }
+
+        public void drawGameBoard()
+        {
+            // upper
+            Console.SetCursorPosition(0, 0);
+            for (int i = 0; i < gameBoardWidth; i++)
+            {
+                Console.Write("#");
+            }
+            // down
+            Console.SetCursorPosition(0, gameBoardHeight);
+            for (int i = 0; i < gameBoardWidth; i++)
+            {
+                Console.Write("#");
+            }
+            //left
+            Console.SetCursorPosition(0, 0);
+            for (int i = 0; i <= gameBoardHeight; i++)
+            {
+                Console.Write("#");
+                Console.SetCursorPosition(0, i);
+            }
+            //right
+            Console.SetCursorPosition(gameBoardWidth, 0);
+            for (int i = 0; i <= gameBoardHeight; i++)
+            {
+                Console.Write("#");
+                Console.SetCursorPosition(gameBoardWidth, i);
+            }
+
+
+
+        }
+    }
+}
